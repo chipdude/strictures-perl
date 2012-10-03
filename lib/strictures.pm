ee7e59517e3181484156ca3d8d7ead472023190e
Chip Salzenberg <chip@topsy.com>
2012-10-02 21:19:22 -0700

    1.4.4.1 packaging

--- lib/strictures.pm
+++ lib/strictures.pm
@@ -5,7 +5,7 @@ use warnings FATAL => 'all';
 
 use constant _PERL_LT_5_8_4 => ($] < 5.008004) ? 1 : 0;
 
-our $VERSION = '1.004004'; # 1.4.4
+our $VERSION = '1.004004001'; # 1.4.4.1
 
 sub VERSION {
   for ($_[1]) {
